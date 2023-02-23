793d858b23876cd11f7d44a0aea0af8d72f75de2
Daniel Marrero <dmarrero@192.168.1.10>
2023-02-23 02:35:34 -0300

    cleanup, search on its own component

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="d-flex">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Find movies by title..."
+        v-model="query"
+      />
+  
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  
+      <button
+        class="btn btn-primary"
+        @click="$emit('handleSearch', query)"
+      >
+        Search
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  const query = ref('');
+</script>
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -2,33 +2,15 @@
   <div class="list-container">
     <img src="../assets/banner.png" alt="Home banner" />
     <LoadingStatus v-if="loading" />
-    <div class="d-flex">
-      <input
-        type="text"
-        class="form-control"
-        placeholder="Find movies by title..."
-        v-model="query"
-      />
-  
-      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-  
-      <button
-        class="btn btn-primary"
-        @click="handleSearch"
-      >
-        Search
-      </button>
-    </div>
+    <SearchBox @handle-search="handleSearch" />
     <br />
-    <div
-      class="alert alert-warning"
-      role="alert"
-      v-if="emptyError"
+    <p v-if="notFound">
+      No movies found.
+    </p>
+    <ul
+      class="list-group"
+      v-if="movies.length"
     >
-      Oops! Make sure you input some text before hitting that Search button :)
-    </div>
-
-    <ul class="list-group">
       <li 
       class="list-group-item"
       role="button"
@@ -44,8 +26,19 @@
       class="d-flex justify-content-between"
       v-if="movies.length"
     >
-      <button class="btn btn-link" @click="() => handlePageChange(page - 1)">Previous</button>
-      <button class="btn btn-link" @click="() => handlePageChange(page + 1)">Next</button>
+      <button
+        class="btn btn-link"
+        @click="() => handlePageChange(page - 1)"
+        :disabled="page < 2"
+      >
+        Previous
+      </button>
+      <button
+        class="btn btn-link"
+        @click="() => handlePageChange(page + 1)"
+      >
+        Next
+      </button>
     </div>
   </div>
 </template>
@@ -54,40 +47,48 @@
   import { ref } from 'vue';
   import { searchMoviesByTitle } from '../client';
   import LoadingStatus from '@/components/LoadingStatus.vue';
+  import SearchBox from '@/components/SearchBox.vue';
 
-  const lastStoredSearch = localStorage.getItem('movies');
-  const lastStoredPage = localStorage.getItem('page');
-  const lastStoredQuery = localStorage.getItem('query');
-
-  const query = ref(lastStoredQuery);
   const loading = ref(false);
-  const emptyError = ref(false);
-  const page = ref(lastStoredPage);
-  const movies = ref(JSON.parse(lastStoredSearch) || []);
+  const notFound = ref(false);
+  const page = ref(1);
+  const movies = ref([]);
+  const s = ref('');
 
-  async function handleSearch() {
-    if (query.value) {
-      emptyError.value = false;
+  async function handleSearch(query) {
+    if (query) {
+      s.value = query;
+    }
+    
+    if (s.value) {
+      movies.value = [];
       loading.value = true;
+      notFound.value = false;
 
-      const response = await searchMoviesByTitle(query.value, page.value);
+      const { Search, Error } = await searchMoviesByTitle(s.value, page.value);
 
-      movies.value = response.Search;
+      if (Search) {
+        movies.value = Search;
+      }
+
+      if (Error) {
+        notFound.value = true;
+      }
 
       // TODO: good place to consider using vuex - along with loading status
-      localStorage.setItem('movies', JSON.stringify(movies.value));
-      localStorage.setItem('page', page.value);
-      localStorage.setItem('query', query.value);
+      // localStorage.setItem('movies', JSON.stringify(movies.value));
+      // localStorage.setItem('page', page.value);
+      // localStorage.setItem('query', query);
       
       loading.value = false;
     } else {
-      emptyError.value = true;
+      // TODO: show some error when user tries to search an empty string
     }
   }
 
   function handlePageChange(pageNumber) {
-    page.value = pageNumber > 0 ? pageNumber : 1;
-    pageNumber > 0 && handleSearch();
+    page.value = pageNumber;
+    handleSearch();
   }
 </script>
 
