c40be0cddbde110e958cc46ead4ff47d95827e16
Daniel Marrero <dmarrero@192.168.1.10>
2023-02-23 00:02:36 -0300

    movie finder app

---
+++ src/components/LoadingStatus.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="spinner-border text-primary"
+    role="status"
+  >
+    <span
+      class="visually-hidden"
+    >
+      Loading...
+    </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LoadingStatus',
+  }
+</script>
+
+<style scoped>
+  div {
+    left: 1rem;
+    position: fixed;
+    top: 1rem;
+  }
+</style>
